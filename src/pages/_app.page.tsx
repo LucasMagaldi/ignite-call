3938e527e3a1ac4390234edad5902264846044cd
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-09-01 09:41:06 -0300

    feat: extent prettier into eslint config file

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import { globalStyle } from '../../styles/globals'
+import { globalStyle } from '../styles/globals'
 
 globalStyle()
 
--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,14 +1,14 @@
-import { globalCss } from "@ignite-ui/react";
+import { globalCss } from '@ignite-ui/react'
 
 export const globalStyle = globalCss({
   '*': {
     boxSizing: 'border-box',
     margin: 0,
-    padding: 0
+    padding: 0,
   },
 
   body: {
     backgroundColor: '$gray900',
-    color: '$gray100'
-  }
+    color: '$gray100',
+  },
 })
