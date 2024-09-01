5eda0597664648464c2d9e6a98d82be4552f152f
Lucas Magaldi <lucas.magaldi@hotmail.com>
2024-09-01 11:59:52 -0300

    feat: add react hook form into username form

--- src/pages/home/components/UsernameForm/index.tsx
+++ src/pages/home/components/UsernameForm/index.tsx
@@ -2,9 +2,24 @@ import { TextInput } from '@ignite-ui/react'
 import { Form } from './style'
 import { ArrowRight } from 'phosphor-react'
 
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const UsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type UsernameFormData = z.infer<typeof UsernameFormSchema>
+
 export default function UsernameForm() {
+  const { register, handleSubmit } = useForm<UsernameFormData>()
+
+  async function handleUsernameFormSubmit(data: UsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleUsernameFormSubmit)}>
       <TextInput
         prefix="ignite.com/"
         placeholder="your user"
@@ -13,6 +28,7 @@ export default function UsernameForm() {
         crossOrigin={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
+        {...register('username')}
       />
       <button type="submit">
         Book
